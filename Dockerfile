65cca3bb7dd7de1f38f1cc13ccafc3a865400fa2
Giorgi Zeikidze <gizeik@gmail.com>
2021-05-07 14:23:51 +0300

    complete app functionality

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM ubuntu
 RUN apt-get update -y && \
     apt-get install -y python3-pip python3-flask python3 gunicorn && \
-    pip3 install Werkzeug boto3 && \
+    pip3 install boto3 && \
     echo $HOME
 WORKDIR /app
 COPY credentials /root/.aws/credentials
 COPY main.py /app/main.py
 COPY upload.html.j2 /app/templates/upload.html.j2
+COPY redirect.html /app/templates/redirect.html.j2
+
 CMD ["gunicorn", "--workers", "1", "--bind", "[::]:5000", "main:app", "--timeout", "90"]
 #EXPOSE 5000/tcp
--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def upload_file():
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_files():
     if request.method == 'POST':
-        f = request.files['file']
-        client.put_object(Key=f.filename, Body=f.stream, Bucket=bucket)
-        return 'file uploaded successfully'
+        file = request.files['file']
+        client.put_object(Key=file.filename, Body=file.stream, Bucket=bucket)
+        return render_template('redirect.html.j2')
 
 @app.route('/download/<key>')
 def download_file(key):
     file = client.get_object(Key=key ,Bucket=bucket)
     return Response(
         file['Body'].read(),
-        mimetype='text/plain',
+        mimetype=file['ContentType'],
         headers={"Content-Disposition": "attachment", "filename": key})
